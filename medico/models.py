d47bea8df8c226b69f5c3991c7c85f8435ee2c02
DEV-T-2025 <arthuramerico33@gmail.com>
2025-08-28 15:42:47 -0300

    Criação dos models Medico e Especialidade, admin configurado

--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,16 @@
+
 from django.db import models
 
-# Create your models here.
+class Especialidade(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Medico(models.Model):
+    nome = models.CharField(max_length=100)
+    crm = models.CharField(max_length=20)
+    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nome} - {self.crm}"
